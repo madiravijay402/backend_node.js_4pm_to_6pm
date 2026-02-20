9b8611846f3ef638dab7dfefb6873b685e3e15e5
madiravijay402 <vijaymadira681@gmail.com>
2026-02-20 10:41:29 +0530

    commit

---
+++ classWork/day2.js
@@ -0,0 +1,5 @@
+// console.log(a);
+let a=40;
+console.log(a);
+
+// here a is hoisted but not initialized js kn iws that a is there but until let a=40 intialized it will sow eroor this applies to const as well 
