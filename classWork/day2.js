666552a3de2de16eeebeb7900d4fc48d9c4cefd7
madiravijay402 <vijaymadira681@gmail.com>
2026-02-20 17:34:34 +0530

    commit

--- classWork/day2.js
+++ classWork/day2.js
@@ -4,6 +4,33 @@
 
 // here a is hoisted but not initialized js kn iws that a is there but until let a=40 intialized it will sow eroor this applies to const as well 
 
-console.log(b); // undefined
-var b = 10;    
-const 
+// console.log(b); // undefined
+// var b = 10;    
+const add=(a,b)=>{
+     console.log(a+b);
+     
+}
+   // will 
+// console.log(add(10,30));
+add(10,30);
+
+const noPara=()=>{
+    console.log("hello");
+}
+noPara();
+
+const onePara=(a)=>{
+    console.log(a);
+}
+onePara("vanakam");
+
+const multiPara=(a,b,c)=>{
+    return a+b+c;
+}
+console.log(multiPara(12,3,29));
+
+const multiState=(a,b)=>{
+    let result=a+b
+    return result;
+}
+console.log(multiState(10,20));
