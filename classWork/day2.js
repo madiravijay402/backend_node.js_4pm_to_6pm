6ea458387a201fb7b8723c7d0beada0b0551005b
madiravijay402 <vijaymadira681@gmail.com>
2026-02-20 16:03:09 +0530

    update

--- classWork/day2.js
+++ classWork/day2.js
@@ -1,5 +1,8 @@
+// // console.log(a);
+// let a=40;
 // console.log(a);
-let a=40;
-console.log(a);
 
 // here a is hoisted but not initialized js kn iws that a is there but until let a=40 intialized it will sow eroor this applies to const as well 
+
+console.log(b); // undefined
+var b = 10;    
