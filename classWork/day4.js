c62e6ced40f6b654668cc900b01adfde79fd263d
madiravijay402 <vijaymadira681@gmail.com>
2026-02-23 10:28:59 +0530

    commit

--- classWork/day4.js
+++ classWork/day4.js
@@ -9,3 +9,15 @@ copy.name="ram";
 console.log(copy.name);
 console.log(empDetails.name);
 
+const obj=(JSON.stringify(empDetails));
+// obj.name="kumar";
+console.log(obj);
+console.log(empDetails.name);
+
+// const deepCopy = _.cloneDeep(empDetails); // instal lodash library npm install lodash
+// deepCopy.name="kumar";
+// console.log(deepCopy.name);
+// console.log(empDetails.name);  
+
+const {name,...rest}= empDetails;
+console.log(rest);
