391e84d7adedb82bad4de4c3b9ace12dd9126183
madiravijay402 <vijaymadira681@gmail.com>
2026-02-23 15:05:27 +0530

    update

--- classWork/day4.js
+++ classWork/day4.js
@@ -1,23 +1,37 @@
-let empDetails={
+// let empDetails={
+//     name:"vijay",
+//     age:22,
+//     city:"chennai",
+//     salary:25000
+// }
+// const copy = structuredClone(empDetails);
+// copy.name="ram";
+// console.log(copy.name);
+// console.log(empDetails.name);
+
+// const obj=(JSON.stringify(empDetails));
+// // obj.name="kumar";
+// console.log(obj);
+// console.log(empDetails.name);
+
+// // const deepCopy = _.cloneDeep(empDetails); // instal lodash library npm install lodash
+// // deepCopy.name="kumar";
+// // console.log(deepCopy.name);
+// // console.log(empDetails.name);  
+
+// const {name,...rest}= empDetails;
+// console.log(rest);
+
+const orginal={
     name:"vijay",
-    age:22,
-    city:"chennai",
-    salary:25000
+    address:{
+        city:"chennai"
+    },
+    hobbies:["cricket","football"]
 }
-const copy = structuredClone(empDetails);
+
+const copy={...orginal}; 
 copy.name="ram";
-console.log(copy.name);
-console.log(empDetails.name);
-
-const obj=(JSON.stringify(empDetails));
-// obj.name="kumar";
-console.log(obj);
-console.log(empDetails.name);
-
-// const deepCopy = _.cloneDeep(empDetails); // instal lodash library npm install lodash
-// deepCopy.name="kumar";
-// console.log(deepCopy.name);
-// console.log(empDetails.name);  
-
-const {name,...rest}= empDetails;
-console.log(rest);
+console.log(orginal.name);
+copy.address.city="banglore";
+console.log(orginal.address.city); // here adress is not copied by value but by reference so it will change in orginal    
